39f6605dd5a76f4cd629bfb63bed5b2fd1840e7a
serenasconci <62289386+serenasconci@users.noreply.github.com>
2020-06-04 12:52:46 +0200

    Update R_code_point_patterns.r

--- R_code_point_patterns.r
+++ R_code_point_patterns.r
@@ -1,16 +1,16 @@
-##################CODICE PER ANALISI DEI POINT PATTERNS
+###CODICE PER ANALISI DEI POINT PATTERNS
 
 install.packages("ggplot2")
 # per richiamare il pacchetto library() oppure require()
 install.packages("spatstat")
 
-#pacchetti utilizzati in questa lezione
+#PACCHETTI UTILIZZATI
 library(ggplot2)
 library(spatstat)
 
 setwd("C:/lab")
 
-#per importare i dati
+#PER IMPORTARE I DATI IN FORMATO TABELLA
 covid <- read.table("covid_agg.csv", head=T)
 
 head(covid)
@@ -25,10 +25,12 @@ plot(covid$country,covid$cases,las=0) #etichette parallele (labels)
 plot(covid$country,covid$cases,las=1) #etichette orizzontali
 plot(covid$country,covid$cases,las=2)#etichette perpendicolari all'asse
 plot(covid$country,covid$cases,las=3) #etichette verticali
-#las=n serve per cambiare il verso delle etichette
+#las=n SERVE PER CAMBIARE IL MODELLO DI ETICHETTA
 
 #cex:character exageration
 plot(covid$country,covid$cases,las=3,cex.lab=0.5, cex.axis=0.5)
+#cex.lab DEFINISCE LA DIMENSIONE DEL CARATTERE DELLE ETICHETTE
+#cex.axis DEFINISCE LA DIMENSIONE DEL CARATTERE DEGLI ASSI 
 
 #visualizzazione spaziale
 
@@ -36,30 +38,37 @@ plot(covid$country,covid$cases,las=3,cex.lab=0.5, cex.axis=0.5)
 data(mpg)
 head(mpg)
 
-#ggplot di esempio: data, aes (aestethics: variabili che comporranno l'estetica finale del grafico), tipo di geometria 
+#ggplot di esempio
+#data, aes (aestethics: VARIABILI CHE COMPORRANNO L'ESTETICA FINALE DEL GRAFICO), tipo di geometria 
 ggplot(mpg,aes(x=displ,y=hwy))+geom_point()
-#cambio di geometria, linee
-ggplot(mpg,aes(x=displ,y=hwy))+geom_line()
-#cambio geometria, poligoni
-ggplot(mpg,aes(x=displ,y=hwy))+geom_polygon()
+#CAMBIO DI GEOMETRIA=PUNTI
 
-#grafico covid, ggplot
+ggplot(mpg,aes(x=displ,y=hwy))+geom_line()
+#CAMBIO DI GEOMETRIA=LINEE
+
+ggplot(mpg,aes(x=displ,y=hwy))+geom_polygon()
+#CAMBIO DI GEOMETRIA=POLIGONI
+
+#ggplot di covid
 names(covid)
 ggplot(covid,aes(x=lon,y=lat,size=cases))+geom_point()
-#esercizio density: densità di punti presenti in una certa area
 
+#ESERCIZIO density: DENSITA' DI PUNTI PRESENTI IN UNA CERTA AREA
 library(spatstat)
 #creare data set per spatstat
 attach(covid)
 covids <- ppp(lon, lat, c(-180,180), c(-90,90))
-
+#LA FUNZIONE ppp CREA UNA CLASSE, RAPPRESENTA UN SET DI DATI DEL MODELLO A PUNTI NEL PIANO BIDIMENSIONALE
+#LON MAX E MIN (-180,180)
+#LAT MAX E MIN (-90,90)
 d <- density(covids)
 plot(d)
 
 ##########################per inserie i punti: POINTS PATTERN2
-#pacchetti utilizzati
+#PACCHETTI UTILIZZATI
 install.packages("rgdal")
 library(rgdal)
+library(spatstat)
 
 points(covids, pch=19)
 plot(d)
@@ -70,33 +79,37 @@ points(covid)
 setwd("C:/lab")
 
 load(".RData")
-# per caricare i point pattern in Rdata
+#LA FUNZIONE load() SERVE PER CARICARE I POINT PATTERN IN RData
 
 ls()
-# fa vedere la lista dei vari file
+# PERMETTE DI VEDERE LA LISTA DEI VARI FILES DELL'RData
 
-library(spatstat)
 plot(d)
 
+#PER CAMBIARE LA GAMMA DI COLORI VIENE USATA LA FUNZIONE colorRampPalette() 
+
 cl <- colorRampPalette(c('yellow','orange','red')) (100)
+#IN QUESTO CASO I MINIMI VALORI VERRANNO RAPPRESENTATI IN GIALLO MENTRE I MASSIMI VERRANNO RAPPRESENTATI IN ROSSO
+#(100) INDICA IL NUMERO DELLE GRADAZIONI DI COLORI PRESENTI NELLA PALETTE
 plot(d, col=cl)
-#per cambiare la gamma dei colori: palette(in questo caso i minimi valori verranno rappresentati in giallo, i massimi in rosso)
-#(100) indica le varie gradazioni
+#PLOT DELLA DENSITA' CON USO DELLA PALETTE DENOMINATA cl
 
 #ESERCIZIO: plot della mappa della densità dal verde al blu.
 cl <- colorRampPalette(c('green','yellow','blue')) (150)
 plot(d,col=cl)
 
-#per aggiungere i punti del covid si utilizza points()
+#PER AGGIUNGERE I PUNTI DEL COVID SI UTILIZZA LA FUNZIONE POINT()
 points(covids)
 
-#caricare dei dati dall'esterno 
+
 library(rgdal)
-# per inserire i confini dei vari stati.
+#PERMETTE DI CARICARE DEI DATI DALL'ESTERNO
+
 coastlines <- readOGR("ne_10m_coastline.shp")
+# PER INSERIRE I CONFINI DEI VARI STATI
 
 plot(coastlines, add=T)
-#add serve ad aggiungere al plot precedente il nuovo plot, ovvero le coastlines.
+#add AGGIUNGE AL PLOT PRECEDENTE UN NUOVO PLOT, IN QUESTO CASO LE COASTLINES 
 
 #ESERCIZIO: plot della mappa di densità con una nuova colorazione ed aggiunta delle coastlines
 plot(d)
@@ -121,26 +134,29 @@ plot(coastlines, add=T)
 
 #Interpolazione
 head(covid)
-#per visualizzare l'intera tabella
-View(covid)
 
-#marks del point pattern
+View(covid)
+#PER VISUALIZZARE L'INTERA TABELLA
+
 marks(covids) <- covid$cases
-#Smooth() mappa continua dei vari punti di covid in base ai casi
+#ESTRAE IL POINT PATTERN (CASES) E LO ASSOCIA A COVID 
+
+#Smooth()FUNZIONE CHE PERMETTE DI OTTENERE UNA MAPPA CONTINUA DEI VARI PUNTI DI COVID IN BASE AI CASI
 s <- Smooth(covids)
-#s è la mappa dei casi
+#s E' LA MAPPA DEI CASI
 plot(s)
 
-#Esercizio:plot(s) with points and coastlines
+#ESERCIZIO:plot(s) with points and coastlines
 cl5 <- colorRampPalette(c('cyan', 'purple', 'red')) (200) 
 plot(s, col=cl5, main="estimate of cases")
 points(covids)
 coastlines <- readOGR("ne_10m_coastline.shp")
 plot(coastlines, add=T)
-#chiudere il grafico
-dev.off()
 
-##### mappa finale
+dev.off()
+#PER CHIUDERE IL GRAFICO
+
+# mappa finale
 par(mfrow=c(2,1))
 
 # densità dei punti di covid
@@ -157,9 +173,9 @@ points(covids)
 coastlines <- readOGR("ne_10m_coastline.shp")
 plot(coastlines, add=T)
 
- #la maggior parte dei casi sono stati presi in situ
+ #LA MAGGIOR PARTE DEI CASI VENGONO PRESI IN SITU
 
-########## San Marino
+###TESI San Marino
 
 setwd("C:/lab")
 load("Tesi.RData")
@@ -167,9 +183,12 @@ head(Tesi)
 
 #plot della densità
 library(spatstat)
-#si allega la tabella
+
 attach(Tesi)
+#SI ALLEGA LA TABELLA
+
 summary(Tesi)
+#FUNZIONE CHE FA VISUALIZZARE UN RIEPILOGO
 
 #point pattern: x(lon), y(lat), c(xmin, xmax) , c(ymin, ymax)
 
@@ -186,49 +205,52 @@ points(Tesippp, col="black")
 
 
 setwd("C:/lab/")
-#le virgolette servono a caricare file esterni
+#LE VIRGOLETTE SERVONO A CARICARE FILES DALL'ESTERNO 
 load("sanmarino.RData")
 ls()
 #dT:mappa di densità ; Tesi: dataset originale ; Tesippp: point pattern (Longitudine e Latitudine) 
 
-#associare i valori che vogliamo stimare nello spazio (interpolazione)
+#ASSOCIARE I VALORI CHE VOGLIAMO STIMARE NELLO SPAZIO (interpolazione)
 
 library(spatstat)
-#required(): altro modo per richiamare il pacchetto
+#required(): MODALITA' ALTERNATIVA DI RICHIAMARE UN PACCHETTO
 
 plot(dT)
 points(Tesippp, col="green")
-#la densità è direttamente proporzionata ai prati aridi 
+#LA DENSITA' E' DIRETTAMENTE PROPORZIONATA AI PRATI ARIDI
 
 head(Tesi)
 
-#associa i valori della variabile al point pattern marks()
-#prendere i singoli punti di campionamento ed associarli ai singoli punti del point pattern, in questo caso species richness
 marks(Tesippp) <- Tesi$Species_richness
+#marks() ASSOCIA I VALORI DELLA VARIABILE AL POINT PATTERN
+#PRENDERE I SINGOLI PUNTI DI CAMPIONAMENTOED ASSOCIARLI AI SINGOLI PUNTI DEL POINT PATTERN
+#IN QUESTO CASO SPECIES RICHNESS
 
-#per vedere l'utilizzo della singola funzione va inserito su R il nome della funzione
+#PER VEDERE L'UTILIZZO DI UNA SINGOLA FUNZIONE VA INSERITO SU R IL NOME DELLA FUNZIONE 
 
-#interpolazione: 
+#interpolazione
 interpol <- Smooth(Tesippp)
 #plottare la mappa ed inserire i punti
 plot(interpol)
 points(Tesippp, col="green")
 
-#i valori di richezza specifica piu bassi sono nella parte a nord e sud ovest del data set, più alti ovest e sud-est (RISCRIVI CON REGISTRAZIONE)
+#I VALORI DI RICCHEZZA SPECIFICA PIU' BASSI SONO NELLA PARTE A NORD E SUD-OVEST DEL DATA SET
+#I VALORI PIU' ALTI SONO AD OVEST E SUD-EST
 
 library(rgdal) # per leggere i file vettoriali
 sanmarino <- readOGR("San_Marino.shp")
 
 plot(sanmarino)
 
-#per aggiungere mappa interpol va aggiunto add=TRUE
+#PER AGGIUNGERE LA MAPPA interpol VA AGGIUNTO add=TRUE
 plot(interpol, add=TRUE)
-#aggiungere i punti
+#PER AGGIUNGERE I PUNTI
 points(Tesippp,col="green")
 
 plot(sanmarino, add=TRUE)
-#Esercizio:plot multiframe di densità e interpolazione
-#par() crea un multiframe
+
+#ESERCIZIO:plot multiframe di densità e interpolazione
+#par() CREA UN MULTIFRAME
 par(mfrow=c(2,1))
 #densità
 plot(dT,main="Density of points")
@@ -237,9 +259,9 @@ points(Tesippp, col="black")
 plot(interpol, main="Estimate of species richness")
 points(Tesippp, col="black")
 
-#main= ; inserisce il titolo al grafico
+#main= INSERISCE IL TITOLO AL GRAFICO
 
-#Esercizio::plot multiframe di densità e interpolazione con due colonne ed una riga 
+#ESERCIZIO:plot multiframe di densità e interpolazione con due colonne ed una riga 
 par(mfrow=c(1,2))
 #densità
 plot(dT,main="Density of points")
