7ae4957782264346a09286d39a02c480e4d6225a
serenasconci <62289386+serenasconci@users.noreply.github.com>
2020-06-05 10:42:55 +0200

    Update R_code_landcover.r

--- R_code_landcover.r
+++ R_code_landcover.r
@@ -4,7 +4,7 @@ setwd("C:/lab")
 
 library(raster)
 
-#brick() importare un'immagine satellitare con tutti i dati
+#brick()FUNZIONE CHE PERMETTE DI IMPORTARE UN'IMMAGINE SATELLITARE CON TUTTI I DATI
 
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 
@@ -17,34 +17,28 @@ library(RStoolbox)
 #rgb 
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 
-#
-p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=4)
 
-# inserendo il nome dell'immagine p224r63_2011c si ottengono tutte le informazioni
-#$ simbolo di unione
-#map è la mappa che il plot ha generato
+p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=4)
+#unsuperClass() CLASSIFICAZIONE SENZA SUPERVISIONE
+#IMMAGINE PIU' NUMERO DI CLASSI DESIDERATE
+
+p224r63_2011
+# INSERENDO IL NOME DELL'IMMAGINE p224r63_2011c SI OTTENGONO TUTTE LE INFORMAZIONI
+
 plot(p224r63_2011c$map)
+#$ SIMBOLO ALTERNATIVO ALLA FUNZIONE ATTACH
+#MAP E' LA MAPPA CHE IL PLOT HA GENERATO
 
 #cambiare i colori della mappa 
 clclass <- colorRampPalette(c('green', 'red', 'blue', 'black'))(100) 
 plot(p224r63_2011c$map, col=clclass)
-#diminuiamo il numero di classi a 2
-p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=2)
-plot(p224r63_2011c$map)
-
 #in funzione del numero di classi, aumenta l'incertezza dell' algoritmo automatico di classificazione
-
 #riportando potenzialmente classi leggermente differenti
 
 p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=4)
 plot(p224r63_2011c$map)
 
+#DIMINUIAMO IL NUMERO DI CLASSI (2)
 p224r63_2011c <- unsuperClass(p224r63_2011, nClasses=2)
 plot(p224r63_2011c$map)
-
-
-
-
-
-
 
